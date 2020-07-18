ff89f3094150f1931ada0d2bc73057cf9384a5e0
Justin Sitter <justin@jaytria.com>
2020-07-18 00:00:52 -0700

    Download and save NOAA data

--- data_downloader.py
+++ data_downloader.py
@@ -1,14 +1,21 @@
-import urllib.request, json, requests
+#!/usr/bin/python3
+import urllib.request, json, os.path as path
 
 def download_functional_groups():
-    functional_group_address = 'https://www.webapps.nwfsc.noaa.gov/apex/parr/functional_group_membership/data/page/'
-    json_data = data_downloader(functional_group_address)
-    save_file("functional_groups.json", json_data)
+    save_filename = "functional_groups.json"
+    if not path.isfile(save_filename):
+        print("Downloading NOAA Functional Group Members Dataset")
+        functional_group_address = "https://www.webapps.nwfsc.noaa.gov/apex/parr/functional_group_membership/data/page/"
+        json_data = data_downloader(functional_group_address)
+        save_file(save_filename, json_data)
 
 def download_diets():
-    diet_address = 'https://www.webapps.nwfsc.noaa.gov/apex/parr/west_coast_fish_mammal_bird_diets_by_functional_group/data/page/'
-    json_data = data_downloader(diet_address)
-    save_file("diets.json", json_data)
+    save_filename = "diets.json"
+    if not path.isfile(save_filename):
+        print("Downloading Diets Dataset")
+        diet_address = "https://www.webapps.nwfsc.noaa.gov/apex/parr/west_coast_fish_mammal_bird_diets_by_functional_group/data/page/"
+        json_data = data_downloader(diet_address)
+        save_file("diets.json", json_data)
 
 def data_downloader(data_url):
     with urllib.request.urlopen(data_url) as address:
@@ -17,11 +24,9 @@ def data_downloader(data_url):
     return data
 
 def save_file(filename, json_data):
-    # Serialize
-    json_obj = json_data.dumps(data, indent=2)
     with open(filename, 'w') as file_bytes:
-        file_bytes.write(json_obj)
+        json.dump(json_data, file_bytes)
 
 if __name__ == "__main__":
     download_functional_groups()
-    # download_diets()
+    download_diets()
